f8bf812af79eba3c27915718ee1587e33731ac70
achrafdakir <achraf01dakir@gmail.com>
2023-09-20 19:24:25 +0100

    return to the first main.h

--- main.h
+++ main.h
@@ -3,27 +3,6 @@
 
 #include <stdio.h>
 #include <stdarg.h>
-#include <stdlib.h>
-#include <limits.h>
-#include <unistd.h>
 
-/**
- * struct format - match the conversion specifiers for printf
- * @id: type char pointer of the specifier i.e (l, h) for (d, i, u, o, x, X)
- * @f: type pointer to function for the conversion specifier
- *
- */
-
-typedef struct format
-{
-	char *id;
-	int (*f)();
-} convert_match;
-
-
-int _putchar(char c);
 int _printf(const char *format, ...);
-
-
-
 #endif
